567fa19444bf0fdd78c50e08c72a2316f440f66f
dunekosh <moguzyaka@gmail.com>
2020-05-15 15:07:46 +0500

    Added login verification tests in the header of the site in base_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class LoginPageLocators:
@@ -14,5 +15,7 @@ class ProductPageLocators:
     BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
     PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
     NAME = (By.CSS_SELECTOR, '.product_main h1')
-    ALERT_LIST = (By.CSS_SELECTOR, '.alertinner strong')
+    ALERT_PRICE = (By.CSS_SELECTOR, '.alert-info strong')
+    ALERT_NAME = (By.CSS_SELECTOR, '#messages .alert-success:nth-child(1) strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages .alert-success:nth-child(1)')
 
--- pages/product_page.py
+++ pages/product_page.py
@@ -8,11 +8,23 @@ class ProductPage(BasePage):
         basket.click()
 
     def compare_name(self):
-        alert_name = self.browser.find_elements(*ProductPageLocators.ALERT_LIST)[0].text
+        alert_name = self.browser.find_element(*ProductPageLocators.ALERT_NAME).text
+        print(alert_name)
         name = self.browser.find_element(*ProductPageLocators.NAME).text
         assert alert_name == name, "The name in the basket does not match"
 
     def compare_price(self):
-        alert_price = self.browser.find_elements(*ProductPageLocators.ALERT_LIST)[2].text
+        alert_price = self.browser.find_element(*ProductPageLocators.ALERT_PRICE).text
+        print(alert_price)
         price = self.browser.find_element(*ProductPageLocators.PRICE).text
         assert alert_price == price, "The price in the basket does not match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Successful message does not disappear"
+
+
